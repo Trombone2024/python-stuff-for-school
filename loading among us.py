9d7de01693752eba9e3da2957ddfb8b1226d24d1
Bruh-Not <91141513+Bruh-Not@users.noreply.github.com>
2021-09-23 09:31:40 -0600

    Add files via upload

---
+++ loading among us.py
@@ -0,0 +1,27 @@
+from time import sleep
+
+twoseconds = 2
+onesecond = 1
+point5seconds = 0.5
+
+print('initializing...')
+sleep(twoseconds)
+print('starting sus api...')
+sleep(twoseconds)
+print('sus api loaded')
+sleep(point5seconds)
+print('loading impostor...')
+sleep(onesecond)
+print('imposter loaded')
+sleep(point5seconds)
+print('loading crewmates...')
+sleep(twoseconds)
+print('crewmates loaded')
+sleep(point5seconds)
+print('loading game code...')
+sleep(twoseconds)
+print('loading game assets...')
+sleep(twoseconds)
+print('game assets loaded')
+sleep(onesecond)
+print('game code loaded')
---
+++ loading.py
@@ -0,0 +1,11 @@
+from time import sleep
+
+twoseconds = 2
+
+print('loading...')
+sleep(twoseconds)
+print('loading...')
+sleep(twoseconds)
+print('loading...')
+sleep(twoseconds)
+print('done!')
